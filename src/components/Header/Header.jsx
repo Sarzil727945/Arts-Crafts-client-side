ad2e2647520f593596bfb83ca5f1a5c4fa3953e4
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-17 20:22:04 +0600

    react setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,80 @@
+// import React, { useContext } from 'react';
+import './Header.css';
+// import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+// import { HiOutlineLockClosed } from 'react-icons/hi';
+// import { BiSearch } from 'react-icons/bi';
+// import logo from '../../assets/logo.svg'
+// import { AuthContext } from '../../Provider/AuthProvider';
+import { useNavigate } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
+import { Navbar } from 'react-bootstrap';
+// import { AuthContext } from '../../AuthProvider/AuthProvider';
+
+const Header = () => {
+     // const { user, logOut } = useContext(AuthContext)
+     const navigate = useNavigate();
+
+     // // logOut part start
+     // const handelLogOut = () => {
+     //      logOut()
+     //           .then(() => {
+     //                // Sign-out successful.
+     //           })
+     //           .catch((error) => {
+     //                // An error happened.
+     //           });
+     // }
+     // // logOut part end
+
+     return (
+          <div className='fixed-top'>
+               <Navbar bg="light" expand="lg" className=' mb-2'>
+                    <Container fluid className='container'>
+                         <Navbar.Brand href="#" className='fs-4'>
+                              <img src='..//../assets/react.svg' alt="" />
+                         </Navbar.Brand>
+                         <Navbar.Toggle aria-controls="navbarScroll" />
+                         <Navbar.Collapse id="navbarScroll">
+                              <Nav
+                                   className="mx-auto my-2 my-lg-0"
+                                   style={{ maxHeight: '100px' }}
+                                   navbarScroll
+                              >
+                                   <ActiveLink to="/">Home</ActiveLink>
+                                   {/* {
+                                        (user?.email) && <div>
+                                             <ActiveLink to="/bookings">My Bookings</ActiveLink>
+                                             <ActiveLink to="/about">About</ActiveLink>
+                                             <ActiveLink to="/services">Services</ActiveLink>
+                                        </div>
+                                   } */}
+                                   <ActiveLink to="/blog">Blog</ActiveLink>
+
+                                   {/* <div className=' ms-5 d-flex align-content-center text-center'>
+                                        <div>
+                                             <span className='fs-3 me-3 '><HiOutlineLockClosed /></span>
+                                             <span className='fs-3 me-3'><BiSearch /></span>
+                                        </div>
+                                        <div className='mt-lg-1'><button type="button" className="btn btn-outline-danger py-2 px-3">Appointment</button></div>
+                                   </div> */}
+
+                              </Nav>
+                              <Form className=' d-flex '>
+                                   {/* {
+                                        user ? <div>
+                                             <img title={user.displayName} className='imgStyle me-3' src={user.photoURL} alt="" />
+                                             <Button onClick={handelLogOut} variant="info" className='py-2'>Log Out</Button>
+                                        </div> : <ActiveLink to="/login">Login</ActiveLink>
+                                   } */}
+                              </Form>
+                         </Navbar.Collapse>
+                    </Container>
+               </Navbar>
+          </div>
+     );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Main/Main';
+import Home from './components/Home/Home';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    // errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path:"/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
