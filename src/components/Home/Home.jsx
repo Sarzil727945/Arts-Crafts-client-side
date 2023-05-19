56066c7852d57400359bcc3a36b0e8350b5cab63
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-19 07:18:25 +0600

    gallery part complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Gallery from './Gallery/Gallery';
 
 const Home = () => {
      return (
           <div className='my-5 pt-5'>
                <section className=' pt-5'>
                     <Banner></Banner>
+                    <Gallery></Gallery>
                </section>
           </div>
      );
